293537d5653166db15244da1fb067d7733a4ed96
Ahmed Maged <ahmedmaged20188@gmail.com>
2023-09-21 00:27:06 +0300

    Init

---
+++ theme/page.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default. Please note that
+ * this is the WordPress construct of pages: specifically, posts with a post
+ * type of `page`.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package BME
+ */
+
+get_header();
+?>
+
+	<section id="primary">
+		<main id="main">
+			<video src="https://thebmegroup.com/wp-content/uploads/2022/06/showreel-cutdownn.mp4" autoplay muted loop class="w-full h-screen bg-black object-cover object-center"></video>
+			<div>
+				
+			</div>
+		</main><!-- #main -->
+	</section><!-- #primary -->
+
+<?php
+get_footer();

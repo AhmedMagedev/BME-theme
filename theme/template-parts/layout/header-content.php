ff467c71b15c407838f2f90841f48d8ee457f51c
Ahmed Maged <ahmedmaged20188@gmail.com>
2023-09-21 09:47:40 +0300

    Hero section

--- theme/template-parts/layout/header-content.php
+++ theme/template-parts/layout/header-content.php
@@ -9,28 +9,24 @@
 
 ?>
 
-<header id="masthead">
+<header id="masthead"  class="w-full flex fixed z-30">
 
 	<div>
 		<?php
 		if ( is_front_page() ) :
 			?>
-			<h1><?php bloginfo( 'name' ); ?></h1>
+			<h1><img src="https://thebmegroup.com/wp-content/themes/bme/img/logo.png" alt="<?php bloginfo( 'name' ); ?>" id="logo" class=" mx-auto w-28 h-8 mt-5"></h1>
 			<?php
 		else :
 			?>
-			<p><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+			<p><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><img src="https://thebmegroup.com/wp-content/themes/bme/img/logo.png" alt="<?php bloginfo( 'name' ); ?>" id="logo" class=" mx-auto w-28 h-8 mt-5"></a></p>
 			<?php
 		endif;
+		?>
 
-		$am_description = get_bloginfo( 'description', 'display' );
-		if ( $am_description || is_customize_preview() ) :
-			?>
-			<p><?php echo $am_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-		<?php endif; ?>
 	</div>
 
-	<nav id="site-navigation" aria-label="<?php esc_attr_e( 'Main Navigation', 'bme' ); ?>">
+	<nav id="site-navigation" class="hidden" aria-label="<?php esc_attr_e( 'Main Navigation', 'bme' ); ?>">
 		<button aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'bme' ); ?></button>
 
 		<?php
@@ -45,3 +41,20 @@
 	</nav><!-- #site-navigation -->
 
 </header><!-- #masthead -->
+
+
+<!-- <div id="navbar" class=" w-full h-20 flex fixed z-40 " >
+		<img src="https://thebmegroup.com/wp-content/themes/bme/img/logo.png" alt="logo" id="logo" class=" mx-auto w-28 h-8 mt-5">
+		<img src="https://thebmegroup.com/wp-content/themes/bme/img/menu-btn.svg" alt="menu" class="w-7 h-4 mr-10 mt-7">
+	</div>
+<script>
+	window.onscroll = function() {scrollFunction()};
+
+	function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").style.backgroundColor = "black";
+  } else {
+    document.getElementById("navbar").style.backgroundColor = "";
+  }
+}
+</script> -->
